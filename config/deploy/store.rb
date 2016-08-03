bf17ba151ad137685fd37e3224de93ee02e791c0
georgiy-voldaev <skiffy166@gmail.com>
2016-08-03 14:02:26 +0300

    change config

--- config/deploy/store.rb
+++ config/deploy/store.rb
@@ -1,4 +1,4 @@
 set :deploy_to, '/home/mb/store'
-set :repo_url, 'git@github.com:georgiy-voldaev/store.git/'
+set :repo_url, 'git@github.com:georgiy-voldaev/store.git'
 
 server '185.118.64.158', user: 'mb', roles: %w{app db web}
