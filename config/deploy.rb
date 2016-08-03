27cbc2cb1660af180fb831515222a1b9a63f563f
georgiy-voldaev <skiffy166@gmail.com>
2016-08-03 10:17:55 +0300

    change config

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.5.0'
 
 set :application, 'store'
-set :repo_url, 'git@github.com:resivalex/store.git'
+set :repo_url, 'git@github.com:georgiy-voldaev/store.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
--- config/deploy/store.rb
+++ config/deploy/store.rb
@@ -1,3 +1,5 @@
 set :deploy_to, 'git@github.com:georgiy-voldaev/store.git/'
 
 server '185.118.64.158', user: 'mb', roles: %w{app db web}
+
+set :repo_url, 'git@github.com:georgiy-voldaev/store.git'
