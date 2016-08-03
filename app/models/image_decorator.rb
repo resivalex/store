39f96b1ad1692535862e2aef0de5592606ca6d0e
georgiy-voldaev <skiffy166@gmail.com>
2016-08-03 14:01:19 +0300

    change size photo miniature

--- app/models/image_decorator.rb
+++ app/models/image_decorator.rb
@@ -1,7 +1,7 @@
 Spree::Image.class_eval do
   attachment_definitions[:attachment][:styles] = {
     :mini => '48x48>', # thumbs under image
-    :small => '200x200>', # images on category view
+    :small => '320x320>', # images on category view
     :product => '480x480>', # full product image
     :large => '800x600>' # light box image
   }
