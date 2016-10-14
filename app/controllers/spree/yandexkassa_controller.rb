bff7be6b395018b920eb56a45607db56ca6e4a7b
Иван Решетников <resivalex@gmail.com>
2016-10-15 00:12:18 +0300

    Delete confusing debug output in Spree::YandexkassaController

--- app/controllers/spree/yandexkassa_controller.rb
+++ app/controllers/spree/yandexkassa_controller.rb
@@ -24,7 +24,9 @@ class Spree::YandexkassaController < Spree::BaseController
       redirect_to :back
     else
       # Find payment for Yandexkassa
-      payment = @order.payments.select { |p| p.payment_method.kind_of? Spree::Gateway::YandexKassa and p.can_started_processing? }.first
+      yandex_payments = @order.payment.select { |p| p.payment_method.kind_of? Spree::Gateway::YandexKassa }
+      suitable_payments = @order.payments.select(&:can_started_processing?)
+      paiment = suitable_payments.first
       if payment.nil?
         payment = @order.payments.create(amount: 0, payment_method: @gateway)
       end
@@ -40,12 +42,9 @@ class Spree::YandexkassaController < Spree::BaseController
   end
 
   def check_order
-    logger.debug "[yandexkassa] check_order started"
     @gateway = Spree::Gateway::YandexKassa.current
     if @notification.acknowledge @gateway.options[:password]
-      logger.debug "[yandexkassa] check notification: true"
       order = Spree::Order.find_by number: @notification.item_id
-      logger.debug "[yandexkassa] order #{order.inspect}"
       if !order
         @notification.message = Spree.t :order_not_found
       elsif order.total.to_f < @notification.gross
@@ -53,80 +52,61 @@ class Spree::YandexkassaController < Spree::BaseController
       elsif order.user_id != @notification.customer_id.split.first.to_i
         @notification.message = Spree.t :order_belongs_to_another_user
         # Order with error. Set error code
-        logger.debug "[yandexkassa] order with error #{@notification.message}"
         @notification.set_response 1
       else
         # Do nothing. Order correct.
-        logger.debug "[yandexkassa] order correct"
         @notification.set_response 0
       end
-    else
-      logger.debug "[yandexkassa] check notification: false"
     end
-    logger.debug "[yandexkassa] response #{@notification.response}"
     render text: @notification.response
   end
 
   def payment_aviso
-    logger.debug "[yandexkassa] payment_aviso started"
     @gateway = Spree::Gateway::YandexKassa.current
     if @notification.acknowledge @gateway.options[:password]
-      logger.debug "[yandexkassa] check notification: true"
+
       order = Spree::Order.find_by number: @notification.item_id
       if order
         # TODO надо ли делать транзакцию?
         # robokassa_transaction = Spree::RobokassaTransaction.create
 
         # Find payment for Yandexkassa with status processing and equal amount
-        payment = order.payments.select { |p| p.payment_method.kind_of? Spree::Gateway::YandexKassa and p.processing? and p.amount.to_f == @notification.gross }.first
-        logger.debug "[yandexkassa] payment: #{payment}"
+        similar_payments = order.payments.select do |p|
+          p.payment_method.kind_of?(Spree::Gateway::YandexKassa) && p.processing? && p.amount.to_f == @notification.gross
+        end
+        payment = similar_payments.first
+
         if payment.nil?
-          logger.debug "[yandexkassa] creating payment"
           payment = order.payments.create(amount: @notification.gross,
                                           payment_method: @gateway) #,
           # source: robokassa_transaction)
-          logger.debug "[yandexkassa] payment: #{payment}"
-          logger.debug "[yandexkassa] payment.started_processing!"
           payment.started_processing!
         end
 
         # Complete payment
-        logger.debug "[yandexkassa] payment.complete!"
         payment.complete!
-
         if order.payment_total?
           # Change order state to paid.
-          logger.debug "[yandexkassa] order.update!"
           order.update!
-          logger.debug "[yandexkassa] order.pay!"
           order.pay!
-          logger.debug "[yandexkassa] order.update!"
           order.update!
         end
-
         @notification.set_response 0
       else
-        logger.debug "[yandexkassa] order not found"
         @notification.set_response 1
       end
-    else
-      logger.debug "[yandexkassa] check notification: false"
     end
-    logger.debug "[yandexkassa] response #{@notification.response}"
+
     render text: @notification.response
   end
 
   def success_payment
-
   end
 
   def failed_payment
-
   end
 
   def create_notification
-    logger.debug "[yandexkassa] create_notification"
     @notification = Yandexkassa::Notification.new request.raw_post
-    logger.debug "[yandexkassa] notification: #{@notification.inspect}"
   end
 end
